908f7538ddf49fa54f5576f9488738f5d1defbaa
Nexxed <nexxed@memeware.net>
2020-03-04 21:14:22 +0000

    Update client.lua
    Removed an unused variable from testing

--- client.lua
+++ client.lua
@@ -1,7 +1,5 @@
 RegisterNetEvent("_teleport:setCoords")
 AddEventHandler("_teleport:setCoords", function(coords)
     local ped = PlayerPedId()
-    local veh = GetVehiclePedIsIn(ped, false)
-
     SetPedCoordsKeepVehicle(ped, coords.x, coords.y, coords.z)
 end)
